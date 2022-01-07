e4363b616a23586c7c79ad8c2e3c4e3bed2b9d89
elliottkobelansky <elliottkobelansky@gmail.com>
2022-01-07 16:32:19 -0500

    Update Docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../../src'))
 
 
 # -- Project information -----------------------------------------------------
@@ -29,11 +29,14 @@ author = 'Elliott Kobelansky'
 # ones.
 extensions = [
     'sphinx.ext.napoleon',
+    'sphinx.ext.autodoc'
 ]
 
+
 napoleon_google_docstring = False
-napoleon_use_param = False
 napoleon_use_ivar = True
+napoleon_use_param = False
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -49,7 +52,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
